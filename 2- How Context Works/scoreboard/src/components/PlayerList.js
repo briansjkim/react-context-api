04fa2defe790bdc1d463f10a3aaede01b17dd63a
Brian Kim <briaansjkim@gmail.com>
2021-08-14 12:58:36 -0400

    implemented Consumer component

--- 2- How Context Works/scoreboard/src/components/PlayerList.js
+++ 2- How Context Works/scoreboard/src/components/PlayerList.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { Consumer } from './Context';
 import PropTypes from 'prop-types';
 import Player from './Player';
 
 const PlayerList = (props) => {
   return (
-    <React.Fragment>
-      {props.players.map( (player, index) =>
-        <Player 
-          {...player}
-          key={player.id.toString()} 
-          index={index}
-          changeScore={props.changeScore}
-          removePlayer={props.removePlayer}           
-        />
+    <Consumer>
+      { context => (
+        <React.Fragment>
+        {context.map( (player, index) =>
+          <Player 
+            {...player}
+            key={player.id.toString()} 
+            index={index}
+            // do not replace props w/ context bc context is ONLY this.state.players in App.js
+            changeScore={props.changeScore}
+            removePlayer={props.removePlayer}           
+          />
+        )}
+      </React.Fragment>
       )}
-    </React.Fragment>
+    </Consumer>
   );
 }
 
 PlayerList.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.object),
   changeScore: PropTypes.func.isRequired,
   removePlayer: PropTypes.func.isRequired,
 };
