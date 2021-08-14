20dfe22dcbddcefc564dd3f3f1b3d11350ce464e
Brian Kim <briaansjkim@gmail.com>
2021-08-14 14:02:26 -0400

    changed App into functional component

--- 2- How Context Works/scoreboard/src/components/App.js
+++ 2- How Context Works/scoreboard/src/components/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
 
-class App extends Component {
+const App = () =>{
 // we remove the Provider component from App because
   //  1. we want to move to the Provider component to the top-most level of the app
   // 2. This way, App has a lot less responsibilities to manage and handle which, in turn,
   // makes App easier to read and manage
-  render() {
     return (
       <div className="scoreboard">
         <Header />
@@ -16,7 +15,6 @@ class App extends Component {
         <AddPlayerForm />
       </div>
     );
-  }
 }
 
 export default App;
