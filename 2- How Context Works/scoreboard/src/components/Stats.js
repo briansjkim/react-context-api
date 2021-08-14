d599b158df5d6004a89e8310cdbf61e4d84114f3
Brian Kim <briaansjkim@gmail.com>
2021-08-14 12:59:03 -0400

    added Consumer component and removed props

--- 2- How Context Works/scoreboard/src/components/Stats.js
+++ 2- How Context Works/scoreboard/src/components/Stats.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Consumer } from './Context';
 
-// calculates the total players and total points from the current state
-const Stats = (props) => {
- 
-  const totalPlayers = props.players.length;
-  const totalPoints = props.players.reduce( (total, player) => {
-    return total + player.score;
-  }, 0);
-
+const Stats = () => {
   return (
-    <table className="stats">
-      <tbody>
-        <tr>
-          <td>Players:</td>
-          <td>{ totalPlayers }</td>
-        </tr>
-        <tr>
-          <td>Total Points:</td>
-          <td>{ totalPoints }</td>
-        </tr>
-      </tbody>
-    </table>
+    <Consumer>
+      {/* this function is necessary and it returns the part of the UI you want to render */}
+      { context => {
+          const totalPlayers = context.length;
+          const totalPoints = context.reduce( (total, player) => {
+            return total + player.score;
+          }, 0);
+
+        return (
+          <table className="stats">
+            <tbody>
+              <tr>
+                <td>Players:</td>
+                <td>{ totalPlayers }</td>
+              </tr>
+              <tr>
+                <td>Total Points:</td>
+                <td>{ totalPoints }</td>
+              </tr>
+            </tbody>
+          </table>
+        )
+      }}
+    </Consumer>
   );
 }
-
-Stats.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.shape({
-    score: PropTypes.number
-  })).isRequired
-};
 
 export default Stats;
